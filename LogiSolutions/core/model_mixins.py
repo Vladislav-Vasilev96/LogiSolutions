f82eb6a0ac1d2f80bcb0f69cbbba0e073b37f8d7
Vladislav-Vasilev96 <vladislav.vasilev9601@gmail.com>
2023-07-18 21:53:20 +0300

    update

--- LogiSolutions/core/model_mixins.py
+++ LogiSolutions/core/model_mixins.py
@@ -13,12 +13,14 @@ class ChoicesStringsMixin:
     def max_length(cls):
         return max(len(x.value) for x in cls)
 
-class Gender(ChoicesStringsMixin,ChoicesMixin, Enum):
+
+class Gender(ChoicesStringsMixin, ChoicesMixin, Enum):
     MALE = 'male'
     FEMALE = 'female'
     DO_NOT_SHOW = 'do not show'
 
-class Status(ChoicesStringsMixin,ChoicesMixin, Enum):
+
+class Status(ChoicesStringsMixin, ChoicesMixin, Enum):
     DELIVERED = 'delivered'
     AWAITING_DELIVERY = 'waiting for  delivery'
     WAITING_FOR_LOAD = 'waiting for load'
@@ -27,9 +29,27 @@ class Status(ChoicesStringsMixin,ChoicesMixin, Enum):
 
 
 STATUS_CHOICES = [
-        ('available', 'Available'),
-        ('on_way', 'On the Way'),
-    ]
+    ('available', 'Available'),
+    ('on_way', 'On the Way'),
+]
+
+
+class WEIGHT_CHOICES(ChoicesStringsMixin, ChoicesMixin, Enum):
+    B = '1.4'
+    C = '10'
+    CE = '24'
+
+
+class TypesOfTruck(ChoicesStringsMixin, ChoicesMixin, Enum):
+    TANK_TRUCK = 'Tank truck'
+    FLATBED_TRUCK = 'Flatbed truck'
+    REFRIGERATOR_TRUCK = 'Refrigerator truck'
+    LIGHT_TRUCK = 'Light truck'
+    CAR_CARRIER_TRAILER = 'Car Carrier Trailer'
+    GARBAGE_TRUCK = 'Garbage truck'
+    MAIL_TRUCK = 'Mail truck'
+
+
 class StrFromFieldsMixin:
     str_fields = ()
 
--- LogiSolutions/offers/models.py
+++ LogiSolutions/offers/models.py
@@ -1,34 +1,35 @@
 from django.db import models
 
-from LogiSolutions.core.model_mixins import STATUS_CHOICES
+from LogiSolutions.core.model_mixins import STATUS_CHOICES, WEIGHT_CHOICES, TypesOfTruck
 
 
-# Create your models here.
 class Offers(models.Model):
     pass
 
 
 class Vehicle(models.Model):
+    LICENSE_PLATE_MAX_LENGHT = 20
+    VEHICLE_TYPE_MAX_LENGHT = 100
+    CURRENT_LOCATION_MAX_LENGHT = 150
     license_plate = models.CharField(
-        max_length=20
+        max_length=LICENSE_PLATE_MAX_LENGHT
     )
     vehicle_type = models.CharField(
-        max_length=100
+
+        choices=TypesOfTruck.choices(),
+        max_length=TypesOfTruck.max_length()
     )
-    capacity = models.DecimalField(
+    max_weight = models.DecimalField(
+        choices=WEIGHT_CHOICES.choices(),
         max_digits=10,
         decimal_places=2
     )
     current_location = models.CharField(
-        max_length=255
+        max_length=CURRENT_LOCATION_MAX_LENGHT,
+        null=True,
+        blank=True,
     )
 
-    model = models.CharField(
-        max_length=100
-    )
-    year = models.PositiveIntegerField(
-
-    )
     owner = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE)
@@ -37,6 +38,11 @@ class Vehicle(models.Model):
         max_length=10,
         choices=STATUS_CHOICES,
         default='ready',
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
     )
 
     def __str__(self):
@@ -49,24 +55,30 @@ class Cargo(models.Model):
     )
 
     location = models.CharField(
-
+        null=False,
+        blank=False,
     )
 
     start_location = models.CharField(
-
+        null=False,
+        blank=False,
     )
 
     destination = models.CharField(
+        null=False,
+        blank=False,
         max_length=255
     )
 
-    weight = models.DecimalField(
-        max_digits=10,
-        decimal_places=2
+    total_km = models.IntegerField(
+        null=False,
+        blank=False,
     )
 
-    quantity = models.IntegerField(
-
+    weight = models.DecimalField(
+        choices=WEIGHT_CHOICES.choices(),
+        max_digits=10,
+        decimal_places=2
     )
 
     cargo_type = models.CharField(
@@ -74,15 +86,23 @@ class Cargo(models.Model):
     )
 
     departure_date = models.DateField(
-
+        null=False,
+        blank=False,
     )
 
     arrival_date = models.DateField(
+        null=False,
+        blank=False,
 
     )
 
     status = models.CharField(
         max_length=100
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
     )
 #     owner = models.ForeignKey(
 #     'auth.User',
