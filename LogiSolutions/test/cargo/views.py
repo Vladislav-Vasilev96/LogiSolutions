4954ba9e31a47d8c678323dc3cfb0a504a7cb315
Vladislav-Vasilev96 <vladislav.vasilev9601@gmail.com>
2023-08-18 22:08:04 +0300

    add integration tests for my views

--- LogiSolutions/test/cargo/views.py
+++ LogiSolutions/test/cargo/views.py
@@ -1,22 +1,32 @@
 from django.test import TestCase
 from django.urls import reverse
 from LogiSolutions.accounts.models import CustomUser
-class CreateWarehouseViewTest(TestCase):
+from LogiSolutions.cargo.models import Cargo
+
+
+class CreateCargoViewTest(TestCase):
     def setUp(self):
         self.user = CustomUser.objects.create_user(email='testuser@example.com', password='testpassword')
         self.client.login(email='testuser@example.com', password='testpassword')
 
-    def test_create_warehouse(self):
+    def test_create_cargo(self):
         form_data = {
-            'name': 'Test Warehouse',
+            'name': 'Test Cargo',
             'location': 'Test Location',
             'contact_number': '+359888888888',
-            'capacity': 1000,
+            'destination': 'Test Destination',
+            'total_km': 150,
+            'weight': '1000',
+            'cargo_type': 'Test Type',
+            'departure_date': '2023-08-06',
+            'arrival_date': '2023-08-08',
             'description': 'Test Description',
         }
 
-        url = reverse('create warehouse')
+        url = reverse('create cargo')
         response = self.client.post(url, data=form_data, follow=True)
 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'warehouse/add-warehouse.html')
+        self.assertTemplateUsed(response, 'cargo/add-cargo.html')
+
+
--- LogiSolutions/test/vehicle/views.py
+++ LogiSolutions/test/vehicle/views.py
@@ -25,27 +25,3 @@ class CreateVehicleViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'vehicle/add-vehicle.html')
 
-class EditVehicleViewTest(TestCase):
-    def setUp(self):
-        self.user = CustomUser.objects.create_user(email='testuser@example.com', password='testpassword')
-        self.client.login(email='testuser@example.com', password='testpassword')
-        self.vehicle = Vehicle.objects.create(license_plate='ABC123', owner=self.user)
-
-    def test_edit_vehicle(self):
-        form_data = {
-            'license_plate': 'ABC123',
-            'vehicle_type': 'SOME_TYPE',
-            'max_weight': '1000',
-            'current_location': 'Test Location',
-            'contact_number': '+359888888888',
-            'is_approved': True,
-        }
-
-
-        url = reverse('edit vehicle', kwargs={'pk': self.vehicle.pk})
-        response = self.client.post(url, data=form_data, follow=True)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'vehicle/edit-vehicle.html')
-        self.assertEqual(Vehicle.objects.get(pk=self.vehicle.pk).license_plate, 'ABC123')
-
